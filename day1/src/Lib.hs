03e695c72e2af9053c6628f2aa4ab64d26d214af
Bartłomiej Kuras <bartlomiej.kuras@o2.pl>
2019-12-01 13:59:07 +0100

    Day 1 solutions

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( part1
+    , part2
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+fuelForMass :: Int -> Int
+fuelForMass = flip (-) 2 . (`div` 3)
+
+accFuelForMass :: Int -> Int
+accFuelForMass =
+    sum . takeWhile (> 0) . drop 1 . iterate fuelForMass
+
+part1 :: [Int] -> Int
+part1 = sum . map fuelForMass
+
+part2 :: [Int] -> Int
+part2 = sum . map accFuelForMass
